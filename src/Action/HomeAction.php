f607bc7bbfb24df9b09b89f21573f9a86372bb87
EmilienLetestu <eletestu@gmail.com>
2018-03-31 19:21:40 +0200

    !! add annotation bundle + create first action to check config !!

---
+++ src/Action/HomeAction.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 31/03/2018
+ * Time: 18:48
+ */
+namespace App\Action;
+
+
+
+use App\Responder\HomeResponder;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeAction
+{
+    /**
+     *
+     * @Route("/", name="home")
+     *
+     * @param HomeResponder $responder
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function __invoke(HomeResponder $responder)
+    {
+        return $responder('hello word');
+    }
+}
---
+++ src/Responder/HomeResponder.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 31/03/2018
+ * Time: 18:49
+ */
+
+namespace App\Responder;
+
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeResponder
+{
+    private $twig;
+
+    /**
+     *
+     * HomeResponder constructor.
+     * @param \Twig_Environment $twig
+     */
+    public function __construct(\Twig_Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public function __invoke($result)
+    {
+       return new Response(
+           $this->twig->render('base.html.twig',['title' => $result])
+       );
+    }
+}
